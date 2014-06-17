2b80bfd6e420c94500cb2666629480a6e948f519
Juan Carlos <zalapeach@gmail.com>
2014-06-17 18:56:49 -0500

    [https://trello.com/c/V1q0dzpL/13-1-5-create-badges-admin] Create badge engine
    * Adds specs for badge model
    * Adds shoulda-matchers to spec_helper

--- spec/models/badges_engine/badge_spec.rb
+++ spec/models/badges_engine/badge_spec.rb
@@ -4,4 +4,9 @@ describe BadgesEngine::Badge do
   let(:badge) { create(:badge) }
 
   it { expect(badge).to be_valid }
+
+  context 'validations' do
+    it { expect(badge).to validate_presence_of(:name) }
+    it { expect(badge).to validate_numericality_of(:level).only_integer }
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
+require 'shoulda-matchers'
 
 ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
 Dir[File.join(ENGINE_RAILS_ROOT, 'spec/support/**/*.rb')].each { |f| require f }
