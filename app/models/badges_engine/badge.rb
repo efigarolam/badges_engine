3fc3256c8ce92dc0f3a3ad6241df57831be4b1eb
Juan Carlos <zalapeach@gmail.com>
2014-06-18 11:21:25 -0500

    [https://trello.com/c/V1q0dzpL/13-1-5-create-badges-admin] Create admin badges
    * Adds unique validation to badge model
    * Adds unique index to badge's name

--- app/models/badges_engine/badge.rb
+++ app/models/badges_engine/badge.rb
@@ -1,6 +1,6 @@
 module BadgesEngine
   class Badge < ActiveRecord::Base
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
     validates :level, numericality: { only_integer: true }
   end
 end
--- db/migrate/20140617153637_create_badges_engine_badges.rb
+++ db/migrate/20140617153637_create_badges_engine_badges.rb
@@ -1,7 +1,7 @@
 class CreateBadgesEngineBadges < ActiveRecord::Migration
   def change
     create_table :badges_engine_badges do |t|
-      t.string :name
+      t.string :name, unique: true
       t.string :image
       t.text :description
       t.integer :level
