117cf553f1822a7cf42c0ae5673d7d094ef987c5
Victor Velazquez <victor.velazquez@crowdint.com>
2014-06-19 18:02:20 -0500

    Adding spec/dummy app for travis
    * Remove from gitignore file

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'f31715cf2661be806b8fa6265a769818fee02064ddf2506b9d8676ef97823333a4758bba9e0e483a9f0c6a71cafe6fe4c426834d2995a4c19116c2d8d517dd78'
