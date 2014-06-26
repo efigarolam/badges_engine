a0c44c1d1cc704e1d8ee4d9ac6122c964091c46b
Victor Velazquez <victor.velazquez@crowdint.com>
2014-06-26 13:26:53 -0500

    Remove instance variables
    * Avoid instance variables from views

--- app/controllers/badges_engine/values_controller.rb
+++ app/controllers/badges_engine/values_controller.rb
@@ -2,41 +2,30 @@ require_dependency "badges_engine/application_controller"
 
 module BadgesEngine
   class ValuesController < ApplicationController
-    before_action :set_value, only: [:show, :edit, :update, :destroy]
+    before_action :set_value, only: [:show, :edit, :destroy]
 
-    def index
-      @values = Value.all
-    end
-
-    def show
-    end
-
-    def new
-      @value = Value.new
-    end
-
-    def edit
-    end
+    expose(:value)
 
     def create
-      @value = Value.new(value_params)
-      if @value.save
-        redirect_to @value, notice: 'Value was successfully created.'
+      value = Value.new(value_params)
+      if value.save
+        redirect_to value, notice: 'Value was successfully created.'
       else
         render action: :new
       end
     end
 
     def update
-      if @value.update(value_params)
-        redirect_to @value, notice: 'Value was successfully updated.'
+      value = Value.find(params[:id])
+      if value.update(value_params)
+        redirect_to value, notice: 'Value was successfully updated.'
       else
         render action: :edit
       end
     end
 
     def destroy
-      @value.destroy
+      value.destroy
       redirect_to values_url, notice: 'Value was successfully destroyed.'
     end
 
