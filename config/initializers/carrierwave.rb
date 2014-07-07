5e16e4c0ff92c5403cc92396b6703d47cafe9211
Juan Carlos <zalapeach@gmail.com>
2014-07-07 03:56:25 -0500

    adds conditions to carrierwave for environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: ENV['PROVIDER'],
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory = ENV['FOG_DIRECTORY']
+unless Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: ENV['PROVIDER'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory = ENV['FOG_DIRECTORY']
+  end
 end
 
 if Rails.env.test?
