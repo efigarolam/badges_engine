97b6ea29878024071cac7b98387acec8acb83079
Juan Carlos <zalapeach@gmail.com>
2014-07-07 02:39:50 -0500

    Config to override engine functionality
    * Adds file to gitignore
    * Fix carrierware to use environment variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: '',
-    aws_access_key_id: '',
-    aws_secret_access_key: ''
+    provider: ENV['PROVIDER'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
-  config.fog_directory = 'crowdkardex'
+  config.fog_directory = ENV['FOG_DIRECTORY']
 end
